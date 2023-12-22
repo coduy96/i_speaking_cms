2883e8dcb5e30f3d1a37e9df8419f8170e704c39
Co Duy <coduy96@gmail.com>
2023-12-22 15:56:40 +0700

    change for i_speaking

--- src/App.tsx
+++ src/App.tsx
@@ -17,137 +17,66 @@ import "typeface-space-mono";
 
 // TODO: Replace with your config
 const firebaseConfig = {
-  apiKey: "AIzaSyDIBvd6ulstFqykOFkwkvRdct5mYa8N180",
-  authDomain: "audiobook-7b39b.firebaseapp.com",
-  projectId: "audiobook-7b39b",
-  storageBucket: "audiobook-7b39b.appspot.com",
-  messagingSenderId: "727870458055",
-  appId: "1:727870458055:web:4af5f45e76286b2f8d56e2"
+  apiKey: "AIzaSyBD3H2AmJ4IpEsiTk-0fpzJxsdbBIICeZc",
+  authDomain: "ispeaking-asia.firebaseapp.com",
+  projectId: "ispeaking-asia",
+  storageBucket: "ispeaking-asia.appspot.com",
+  messagingSenderId: "455706099866",
+  appId: "1:455706099866:web:5c4b0a4414ccf1aa879f23"
 };
 
-type AudioBook = {
+type Questions = {
   name: string;
-  price: number;
-  status: string;
+  part: string;
   published: boolean;
-  related_audiobook: EntityReference[];
-  book_cover: string;
-  tags: string[];
   description: string;
-  categories: string[];
-  publisher: {
-    name: string;
-    external_id: string;
-  },
+  topics: string[];
   publish_date: Date
 }
 
-const audioBookSchema = buildSchema<AudioBook>({
-  name: "AudioBook",
+const questionSchema = buildSchema<Questions>({
+  name: "Questions",
   properties: {
     name: {
-      title: "Name",
+      title: "Question",
       validation: { required: true },
       dataType: "string"
     },
-    price: {
-      title: "Price",
-      validation: {
-        required: true,
-        requiredMessage: "You must set a price between 0 and 1000",
-        min: 0,
-        max: 1000
-      },
-      description: "Price with range validation",
-      dataType: "number"
-    },
-    status: {
-      title: "Status",
+    part: {
+      title: "Part",
       validation: { required: true },
       dataType: "string",
-      description: "Should this product be visible in the website",
-      longDescription: "Example of a long description hidden under a tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis bibendum turpis. Sed scelerisque ligula nec nisi pellentesque, eget viverra lorem facilisis. Praesent a lectus ac ipsum tincidunt posuere vitae non risus. In eu feugiat massa. Sed eu est non velit facilisis facilisis vitae eget ante. Nunc ut malesuada erat. Nullam sagittis bibendum porta. Maecenas vitae interdum sapien, ut aliquet risus. Donec aliquet, turpis finibus aliquet bibendum, tellus dui porttitor quam, quis pellentesque tellus libero non urna. Vestibulum maximus pharetra congue. Suspendisse aliquam congue quam, sed bibendum turpis. Aliquam eu enim ligula. Nam vel magna ut urna cursus sagittis. Suspendisse a nisi ac justo ornare tempor vel eu eros.",
+      longDescription: "",
       config: {
         enumValues: {
-          private: "Private",
-          public: "Public"
+          private: "Part 1",
+          public: "Part 2",
+          pending: "Part 3"
         }
       }
     },
     published: ({ values }) => buildProperty({
       title: "Published",
       dataType: "boolean",
-      columnWidth: 100,
-      disabled: (
-        values.status === "public"
-          ? false
-          : {
-            clearOnDisabled: true,
-            disabledMessage: "Status must be public in order to enable this the published flag"
-          }
-      )
+      columnWidth: 100
     }),
-    related_audiobook: {
-      dataType: "array",
-      title: "Related audio book",
-      description: "Reference to self",
-      of: {
-        dataType: "reference",
-        path: "products"
-      }
-    },
-    book_cover: buildProperty({ // The `buildProperty` method is an utility function used for type checking
-      title: "Book cover",
-      dataType: "string",
-      config: {
-        storageMeta: {
-          mediaType: "image",
-          storagePath: "images",
-          acceptedFiles: ["image/*"]
-        }
-      }
-    }),
-    tags: {
-      title: "Tags",
-      description: "Example of generic array",
-      validation: { required: true },
-      dataType: "array",
-      of: {
-        dataType: "string"
-      }
-    },
     description: {
       title: "Description",
       description: "Not mandatory but it'd be awesome if you filled this up",
-      longDescription: "Example of a long description hidden under a tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis bibendum turpis. Sed scelerisque ligula nec nisi pellentesque, eget viverra lorem facilisis. Praesent a lectus ac ipsum tincidunt posuere vitae non risus. In eu feugiat massa. Sed eu est non velit facilisis facilisis vitae eget ante. Nunc ut malesuada erat. Nullam sagittis bibendum porta. Maecenas vitae interdum sapien, ut aliquet risus. Donec aliquet, turpis finibus aliquet bibendum, tellus dui porttitor quam, quis pellentesque tellus libero non urna. Vestibulum maximus pharetra congue. Suspendisse aliquam congue quam, sed bibendum turpis. Aliquam eu enim ligula. Nam vel magna ut urna cursus sagittis. Suspendisse a nisi ac justo ornare tempor vel eu eros.",
       dataType: "string",
       columnWidth: 300
     },
-    categories: {
-      title: "Categories",
+    topics: {
+      title: "Topics",
       validation: { required: true },
       dataType: "array",
       of: {
         dataType: "reference",
-        path: "bookcategories",
+        path: "topics",
         previewProperties: ["title"]
     }
     },
-    publisher: {
-      title: "Publisher",
-      description: "This is an example of a map property",
-      dataType: "map",
-      properties: {
-        name: {
-          title: "Name",
-          dataType: "string"
-        },
-        external_id: {
-          title: "External id",
-          dataType: "string"
-        }
-      }
-    },
+
     publish_date: {
       title: "Publish date",
       dataType: "timestamp"
@@ -155,31 +84,31 @@ const audioBookSchema = buildSchema<AudioBook>({
   }
 });
 
-const chapterSchema = buildSchema({
-  name: "Chapters",
-  properties: {
-    title: {
-      title: "Title",
-      validation: { required: true },
-      dataType: "string"
-    },
-    video: {
-      title: "Audio file",
-      dataType: "string",
-      validation: { required: false },
-      config: {
-        storageMeta: {
-          mediaType: "audio",
-          storagePath: "audiofile",
-          acceptedFiles: ["audio/*"]
-        }
-      }
-    }
-  }
-});
+// const chapterSchema = buildSchema({
+//   name: "Chapters",
+//   properties: {
+//     title: {
+//       title: "Title",
+//       validation: { required: true },
+//       dataType: "string"
+//     },
+//     video: {
+//       title: "Audio file",
+//       dataType: "string",
+//       validation: { required: false },
+//       config: {
+//         storageMeta: {
+//           mediaType: "audio",
+//           storagePath: "audiofile",
+//           acceptedFiles: ["audio/*"]
+//         }
+//       }
+//     }
+//   }
+// });
 
-const bookcategoriesSchema = buildSchema({
-  name: "BookCategories",
+const topicsSchema = buildSchema({
+  name: "topics",
   properties: {
     title: {
       title: "Title",
@@ -199,24 +128,24 @@ export default function App() {
     return ({
       collections: [
         buildCollection({
-          path: "products",
-          schema: audioBookSchema,
-          name: "Audiobooks",
+          path: "questions",
+          schema: questionSchema,
+          name: "Questions",
           permissions: ({ authController }) => ({
             edit: true,
             create: true,
             // we have created the roles object in the navigation builder
             delete: authController.extra.roles.includes("admin")
           }),
-          subcollections: [
-            buildCollection({
-              name: "Chapters",
-              path: "chapters",
-              schema: chapterSchema
-            })
-          ]
+          // subcollections: [
+          //   buildCollection({
+          //     name: "Chapters",
+          //     path: "chapters",
+          //     schema: chapterSchema
+          //   })
+          // ]
         }),
-        buildCollection({path:"bookcategories", schema: bookcategoriesSchema, name: 'Book Categories'})
+        buildCollection({path:"topics", schema: topicsSchema, name: 'Topics'})
 
       ]
     });
